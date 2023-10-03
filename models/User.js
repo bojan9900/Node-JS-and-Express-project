34161add821aa00d17835d0e0399cb84fc436d84
Bojan Krcic <bojankrcic@Bojans-MacBook-Air.local>
2023-10-03 13:54:04 +0100

    Added password hasing

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 //Import mongoose
 const mongoose = require('mongoose');
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt');
 
 //Creating schema for the model
 const userSchema = new mongoose.Schema({
@@ -17,6 +18,20 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters']
     }
 })
+
+/* Fire a hook after new user has been saved to the database
+userSchema.post('save', function (doc, next) {
+    console.log('New user created and saved', doc)
+    next()
+})*/
+
+// Fire a hook before new user has been saved to the database
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
 
 //Creating model
 const User = mongoose.model('user', userSchema);
